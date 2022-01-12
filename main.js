298869e1e4c8992061476c8b32b4d0b3bc9768e0
Jimster280 <razumov_feodor@mail.ru>
2022-01-12 11:45:59 +0300

    fix:финальная правка и создание метода через прототип

--- main.js
+++ main.js
@@ -6,19 +6,22 @@ const widthq = +prompt("Какова его ширина?");
 const bgq = prompt("Какой цвет заднего фона?");
 const fontSizeq = +prompt("Размер шрифта?");
 
-const DomElement = function (selector, hight, width, bg, fontSize) {
+const domElement = function (selector, hight, width, bg, fontSize) {
   this.selector = selector;
   this.hight = hight;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
-  this.createElement = function() {
+  this.createElement();
+};
+
+domElement.prototype.createElement = function () {
     if (this.selector[0] === ".") {
       let div = document.createElement("div");
       let name = this.selector.substr(1);
       div.className = name;
       div.innerHTML = "<h1>Привет!</h1>";
-      div.style.cssText = "background: " + this.bg + ";" + "height: " + this.hight + "px;"+ "width: " + this.width + "px;" + "font-size: " + DomElement.fontSize + "px";
+      div.style.cssText = "background: " + this.bg + ";" + "height: " + this.hight + "px;"+ "width: " + this.width + "px;" + "font-size: " + domElement.fontSize + "px";
       document.body.append(div);
     } else if (this.selector[0] === "#") {
       let div = document.createElement("div");
@@ -30,14 +33,11 @@ const DomElement = function (selector, hight, width, bg, fontSize) {
     } else {
       alert('должно начинаться с . или #');
     }
-  }
-  this.createElement();
-};
+  };
 
-const DomElement1 = new DomElement(selectorq, hightq, widthq, bgq, fontSizeq);
-const DomElement2 = new DomElement( '#gooo', 180, 160, 'blue', 14);
+const domElement1 = new domElement(selectorq, hightq, widthq, bgq, fontSizeq);
+const domElement2 = new domElement( '#gooo', 180, 160, 'blue', 14);
 
 
-DomElement1();
-DomElement2();
+
 
